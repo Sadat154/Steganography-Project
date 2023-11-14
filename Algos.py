824a0a61b583457730e944cb9c34cfc8fe81fbee
Sadat154 <nafissadat6@gmail.com>
2023-11-14 17:24:08 +0000

    Started Bit Conversion

--- Algos.py
+++ Algos.py
@@ -9,10 +9,24 @@ def binary_to_String(message):
     return ''.join(chr(int(x, 2)) for x in message.split())
 
 
+def hide_message(message, imagepath):
+    #Open original image
+    original_image = Image.open(imagepath)
+
+    #Convert the message into binary
+    binary_message = string_to_binary(message)
+
+    #Check if size of message is too large
+    if len(binary_message) > 3 * original_image.size[0] * original_image.size[1]:
+        raise ValueError("Message is too long to be encoded in the image")
+
+
+    #Encode message into image now
+
+
+
 
 if __name__ == '__main__':
-    message_to_hide = 'SHUSH'
-    print(string_to_binary(message_to_hide))
-
-    test = '1010011 1001000 1010101 1010011 1001000'
-    print(binary_to_String(test))
+    message_to_hide = 'Testing 123 4 5 '
+    image_path = 'C:/Users/naf15/Downloads/12x12.jpg'
+    hide_message(message_to_hide, image_path)
