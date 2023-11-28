dd93999ac9bf349941347ebff187cb403c555cba
Sadat154 <nafissadat6@gmail.com>
2023-11-28 14:35:43 +0000

    LSB Implemented

--- Algos.py
+++ Algos.py
@@ -1,56 +1,65 @@
-###Try LSB Algo
-
 from PIL import Image
 
-def string_to_binary(message):
-    return ''.join(format(ord(x), 'b') for x in message)
+def message_to_bin(message):
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    return binary_message
 
-def binary_to_String(message):
-    return ''.join(chr(int(x, 2)) for x in message.split())
+def encode_lsb(original_image_path, secret_message, output_image_path):
+    # Open the original image
+    original_image = Image.open(original_image_path)
 
+    # Convert the secret message to binary
+    binary_message = message_to_bin(secret_message)
 
-def hide_message(message, imagepath, outputpath):
-    #Open original image
-    original_image = Image.open(imagepath)
-
-
-    #Convert the message into binary
-    binary_message = string_to_binary(message)
-    print(binary_message)
-
-
-    #Check if size of message is too large
+    # Check if the message can be encoded in the image
     if len(binary_message) > 3 * original_image.size[0] * original_image.size[1]:
         raise ValueError("Message is too long to be encoded in the image")
 
-
-
-    #Encode message into image now
+    # Encode the message in the image
     data_index = 0
+    for y in range(original_image.size[1]):
+        for x in range(original_image.size[0]):
+            pixel = list(original_image.getpixel((x, y)))
 
-    for height in range(original_image.size[1]):
-        for width in range(original_image.size[0]):
-            pixel = list(original_image.getpixel((width, height))) #Follows cartesian format (x,y)->(width,height): pixel[R, G, B]
-
-            for i in range(3):
+            # Replace the least significant bit with the message data
+            for i in range(3):  # Iterate over RGB components
                 if data_index < len(binary_message):
                     pixel[i] = pixel[i] & ~1 | int(binary_message[data_index])
                     data_index += 1
 
+            original_image.putpixel((x, y), tuple(pixel))
 
-    original_image.save(outputpath)
+    # Save the encoded image
+    original_image.save(output_image_path + ".jpg")
+
+def decode_lsb(encoded_image_path):
+    # Open the encoded image
+    encoded_image = Image.open(encoded_image_path)
+
+    # Extract the hidden message from the image
+    binary_message = ''
+    for y in range(encoded_image.size[1]):
+        for x in range(encoded_image.size[0]):
+            pixel = list(encoded_image.getpixel((x, y)))
+
+            # Extract the least significant bit from each RGB component
+            for i in range(3):
+                binary_message += str(pixel[i] & 1)
+
+    # Convert binary message to string
+    decoded_message = ''.join([chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8)])
+
+    return decoded_message
+
+# Example usage:
+original_image_path = 'C:/Users/naf15/Downloads/12x12.jpg'
+secret_message = 'Hello, this is a secret message!'
+output_image_path = 'C:/Users/naf15/OneDrive/Desktop/Python_Projects/Steganography-Project/Bit-8/helpMe'
+
+# Encode the message
+encode_lsb(original_image_path, secret_message, output_image_path)
 
 
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    message_to_hide = 'TEsting'
-    image_path = 'C:/Users/naf15/Downloads/12x12.jpg'
-    image_output_path = 'C:/Users/naf15/OneDrive/Desktop/Python_Projects/Steganography-Project/Bit-8/test'
-    hide_message(message_to_hide, image_path, image_output_path)
+# Decode the message
+decoded_message = decode_lsb(output_image_path+".JPG")
+print("Decoded Message:", decoded_message)
